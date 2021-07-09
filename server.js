20239123639ef8a95fbf3f9c1f2a8a109c904713
GOGADORO <45120083+GOGADORO@users.noreply.github.com>
2021-07-10 01:35:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const { PythonShell } = require('python-shell')
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3001, () => {
-    console.log('listen to 3001')
-})
+const port = process.env.PORT || 3001;
+app.listen(port, () =>{
+	console.log("Upload Server running on 3001...");
+});
