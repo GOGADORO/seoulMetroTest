df9514911fabb26192c6838fd7eaad3d20d4d581
GOGADORO <45120083+GOGADORO@users.noreply.github.com>
2021-07-10 16:50:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 var http = require('http');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/',(req,res)=>{
 res.status(200).set("Content-Type", "image/svg+xml").send(data)
 	res.sendFile(__dirname + '/index.html')
 })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () =>{
