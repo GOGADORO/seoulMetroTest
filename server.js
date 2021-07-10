84c5020299cb72cb6424abb65b6a0fff81dc705c
GOGADORO <45120083+GOGADORO@users.noreply.github.com>
2021-07-10 21:31:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 var http = require('http');
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 var server = http.createServer(function(req,res){
 	res.setHeader('Content-Type','image/svg+xml')
